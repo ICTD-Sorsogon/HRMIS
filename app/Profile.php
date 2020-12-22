0911d40afe694e32bf77391840776455ad979c8a
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2020-12-22 15:57:48 +0800

    Profile Model and relationships

---
+++ app/Profile.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+   public function position() 
+   {
+       return $this->hasOne(Position::class);
+   }
+
+   public function address()
+   {
+        return $this->hasMany(Address::class);
+   }
+
+   public function experience()
+   {
+        return $this->hasMany(WorkExperience::class);
+   }
+
+   public function children()
+   {
+       return $this->hasMany(Children::class);
+   }
+
+   public function education()
+   {
+       return $this->hasMany(Education::class);
+   }
+
+   public function volunteer()
+   {
+       return $this->hasMany(VoluntaryWork::class);
+   }
+
+   public function eligibilities()
+   {
+       return $this->hasMany(Eligibility::class);
+   }
+}
