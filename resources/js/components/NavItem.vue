3937702d2a5cbba5a87158761128908c04dad2b2
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2021-01-04 16:39:17 +0800

    add collapse and use $emit events

--- resources/js/components/NavItem.vue
+++ resources/js/components/NavItem.vue
@@ -1,23 +1,25 @@
 <template>
 	<li class="nav-item">
-		<a class="nav-link" @click.prevent="toggleSideBar" :class="{ collapsed:!isToggled }" data-toggle="collapse" data-target="#collapseTwo"
+		<a class="nav-link" v-b-toggle="'collapse-' + name" @click.prevent="toggleSideBar" :class="{ collapsed:!isToggled }" data-toggle="collapse" data-target="#collapseTwo"
 			href="index.html"
 			:aria-expanded="isToggled" aria-controls="collapseTwo">
 			<i class="fas fa-fw" :class="this.icon"></i>
 			<span>{{this.name}}</span>
 		</a>
-		<div id="collapseTwo" class="" :class="{collapsing:collapsing, collapse:!collapsing, show:!collapsing && isToggled}" aria-labelledby="headingTwo" data-parent="#accordionSidebar" >
+		<b-collapse :id="'collapse-' + name" >
+		<div id="collapseTwo" class="collapse" :class="{ show:isToggled }" aria-labelledby="headingTwo" data-parent="#accordionSidebar" >
 			<div class="bg-white py-2 collapse-inner rounded">
 				<h6 class="collapse-header">{{this.title}}:</h6>
 				<a v-for="nav in this.items" class="collapse-item" :href="nav.url">{{nav.name}}</a>
 			</div>
 		</div>
+		</b-collapse>
 	</li>
 </template>
 
 <script>
 	export default {
-		props: ['name','title','items', 'updateIndex', 'index', 'icon'],
+		props: ['name','title','items', 'index', 'icon'],
 		data() {
 			return {
 				collapsing: false,
@@ -26,7 +28,7 @@
 		methods: {
 			toggleSideBar() {
 				let id = this.isToggled || this._uid
-				this.updateIndex(id)
+				this.$emit('update-index', id)
 			}
 		},
 		computed: {
@@ -36,6 +38,3 @@
 		},
 	}
 </script>
-
-<style lang="scss" scoped>
-</style>
