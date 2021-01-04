a732e654d520ac81356d10c4bf881c0eb3e715fe
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2021-01-04 16:41:36 +0800

    extracted components: education et al

---
+++ resources/js/components/profile/WorkExperience.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="card shadow">
+		<a class="card-header py-3" data-toggle="collapse" @click.prevent href="#" v-b-toggle.accordion-3 >
+			<h6 class="m-0 font-weight-bold text-primary">Work Experience</h6>
+		</a>
+		<b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+		<div class="card-body">
+		</div>
+		</b-collapse>
+	</div>	
+</template>
+
+<script>
+	export default {
+		
+	}
+</script>
