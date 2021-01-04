707f4a54a1ca0f73b8117902ebef126397690491
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2021-01-04 16:40:24 +0800

    extract Profile arccordion form components

--- resources/js/components/profile/ProfileAccordion.vue
+++ resources/js/components/profile/ProfileAccordion.vue
@@ -8,89 +8,42 @@
 	</div>
 
 	<!-- Employment Status -->
-	<div class="card shadow">
-		<a class="card-header py-3" data-toggle="collapse" @click.prevent href="#" v-b-toggle.accordion-1 >
-			<h6 class="m-0 font-weight-bold text-primary">Employment Status</h6>
-		</a>
-		<b-collapse id="accordion-1" accordion="my-accordion" role="tabpanel">
-		<div class="card-body">
-		</div>
-		</b-collapse>
-	</div>
+	<employment-status></employment-status>
 	
-
 	<!-- Personal Details -->
-	<div class="card shadow">
-		<a class="card-header py-3" data-toggle="collapse" @click.prevent href="#" v-b-toggle.accordion-2 >
-			<h6 class="m-0 font-weight-bold text-primary">Personal Details</h6>
-		</a>
-		<b-collapse id="accordion-2" visible accordion="my-accordion" role="tabpanel">
-		<div class="card-body">
-			<div class="form-row">
-				<!-- Form Group (first name)-->
-				<div class="form-group col-md-4">
-					<label class="small mb-1" for="inputFirstName">First name</label>
-					<input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie">
-				</div>
-				<!-- Form Group (last name)-->
-				<div class="form-group col-md-4">
-					<label class="small mb-1" for="inputLastName">Last name</label>
-					<input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna">
-				</div>
-				<div class="form-group col-md-3">
-					<label class="small mb-1" for="inputLastName">Middle name</label>
-					<input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna">
-				</div>
-				<div class="form-group col-md-1">
-					<label class="small mb-1" for="inputLastName">Suffix</label>
-					<input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna">
-				</div>
-			</div>
-			<div class="form-row">
-				<!-- Form Group (first name)-->
-				<div class="form-group col-md-1">
-					<label class="small mb-1" for="inputFirstName">Age</label>
-					<input class="form-control" id="inputFirstName" type="text" placeholder="Enter your first name" value="Valerie">
-				</div>
-				<!-- Form Group (last name)-->
-				<div class="form-group col-md-4">
-					<label class="small mb-1" for="inputLastName">Last name</label>
-					<input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna">
-				</div>
-				<div class="form-group col-md-3">
-					<label class="small mb-1" for="inputLastName">Middle name</label>
-					<input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna">
-				</div>
-				<div class="form-group col-md-1">
-					<label class="small mb-1" for="inputLastName">Suffix</label>
-					<input class="form-control" id="inputLastName" type="text" placeholder="Enter your last name" value="Luna">
-				</div>
-			</div>
-		</div>
-		</b-collapse>
-	</div>
+	<personal-details></personal-details>
 	
-	<!-- Employment Status -->
-	<div class="card shadow">
-		<a class="card-header py-3" data-toggle="collapse" @click.prevent href="#" v-b-toggle.accordion-3 >
-			<h6 class="m-0 font-weight-bold text-primary">Work Experience</h6>
-		</a>
-		<b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-		<div class="card-body">
-		</div>
-		</b-collapse>
-	</div>	
+	<!-- Work experience -->
+	<work-experience></work-experience>
+
+	<!-- Education -->
+	<education></education>
+	<family-background></family-background>
+	<!-- Address -->
+	<profile-address></profile-address>
+
 
 </div>
 <!-- /.container-fluid -->
 </template>
 
 <script>
+import Education from './Education'
+import EmploymentStatus from './EmploymentStatus'
+import FamilyBackground from './FamilyBackground.vue'
+import PersonalDetails from './PersonalDetails'
+import WorkExperience from './WorkExperience'
+import ProfileAddress from './ProfileAddress'
+
 	export default {
+		components: {
+			PersonalDetails,
+			WorkExperience,
+			Education,
+			EmploymentStatus,
+			FamilyBackground,
+			ProfileAddress
+		}
 		
 	}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
