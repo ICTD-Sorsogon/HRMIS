7d32cd285d1f9af43d0042fafbd048acab63e632
Kenneth Solomon <synecderz@gmail.com>
2021-01-05 13:25:23 +0800

    design login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,31 +1,50 @@
+<html lang="en">
 
-@extends('layouts.layout')
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-@section('content')
+    <title>HRMIS</title>
+    <link rel="icon" href="{{ URL::asset('/img/favicon.ico') }}" type="image/x-icon"/>
+	<link href="/css/app.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
 
     <div class="container">
 
         <!-- Outer Row -->
-        <div class="row justify-content-center">
+        <div class="row justify-content-center" >
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-xl-10 col-lg-12 col-md-9" style="margin-top:10vh">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
+                    <div class="card-body p-0" >
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-5 offset-lg-1 d-none d-lg-block ">
-                                <img src="{{ asset('img/poslogo.png') }}" alt="" style="height:350px; width:350px;">
+                            <div class="col-lg-5 offset-lg-1 d-none d-lg-block "> 
+                                <img src="{{ asset('img/login.png') }}" alt="" style="margin-top:40px"> 
                             </div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                                     </div>
-                                    <form method="POST" action="{{ route('login') }}">
+                                    <form class="user" method="POST" action="{{ route('login') }}">
                                         @csrf
                                         <div class="form-group row">
-                                            <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus> 
+                                            <input id="email" 
+                                                type="email" 
+                                                class="form-control form-control-user"
+                                                @error('email') is-invalid @enderror 
+                                                name="email" value="{{ old('email') }}" 
+                                                required 
+                                                autocomplete="email" 
+                                                autofocus> 
                                             @error('email')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -41,32 +60,32 @@
                                                 </span>
                                             @enderror
                                         </div>
-                                        {{-- <div class="form-group">
+                                        <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
-                                                <input class="form-check-input custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                                <input class="custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                                <label class="form-check-label" for="remember">
+                                                <label class="form-check-label custom-control-label" for="remember">
                                                     {{ __('Remember Me') }}
                                                 </label>
                                             </div>
-                                        </div> --}}
+                                        </div>
                                             <button type="submit" class="btn btn-primary btn-user btn-block">
                                                 {{ __('Login') }}
                                             </button>
 
-                                {{-- @if (Route::has('password.request'))
+                                {{-- @if (route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        {{ __('forgot your password?') }}
                                     </a>
                                 @endif --}}
                                         <hr>
                                     </form>
-                                    {{-- <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
                                         <a class="small" href="register.html">Create an Account!</a>
-                                    </div> --}}
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -79,4 +98,5 @@
 
     </div>
 
-@endsection
+</body>
+</html>
