307ecbf23bf968619175bcc44b64d2bfc8924587
Kenneth Solomon <synecderz@gmail.com>
2021-01-05 09:11:43 +0800

    redirect to dashboard after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
+
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -26,7 +28,10 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected $redirectTo = 'dashboard';
+    
 
     /**
      * Create a new controller instance.
@@ -36,5 +41,10 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout() {
+    Auth::logout();
+    return redirect('/login');
     }
 }
