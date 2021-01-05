47bbd3c31e0eff44dc43cda8ec848c8eaeb5651e
Kenneth Solomon <synecderz@gmail.com>
2021-01-06 07:34:53 +0800

    add get profile details route

--- routes/web.php
+++ routes/web.php
@@ -26,30 +26,22 @@ Route::get('/register', function () {
 });
 Route::get('/password/reset', function () {
     return view('auth.passwords.email');
+});
 
 Route::get('/profiles', 'ProfileController@profiles');
 
 Route::resource('profile', 'ProfileController');
 
-Route::get('/dashboard', function() {
-    return view('Dashboard');
-
-});
-
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function() {
+    Route::get('/dashboard', 'DashboardController@getUser')->name('dashboard');
+
+    Route::get('/profile/{id}/details', 'ProfileController@getDetails')->name('profile-details');
 
     Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
 
     Route::get('/test', 'ProfileController@profiles');
 
     Route::resource('profile', 'ProfileController');
-
-    Route::get('/dashboard', function() {
-        return view('Dashboard');
-    });
-
-
-    // Route::get('/home', 'HomeController@index')->name('home');
 });
