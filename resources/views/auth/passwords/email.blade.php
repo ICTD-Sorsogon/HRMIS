2ebac373c062663c2289b7718cdee9e07a1a0d2d
Kenneth Solomon <synecderz@gmail.com>
2021-01-05 16:43:53 +0800

    forgot password page

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,73 @@
-@extends('layouts.app')
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-                <div class="card-body">
+    <title>HRMIS</title>
+    <link rel="icon" href="{{ URL::asset('/img/favicon.ico') }}" type="image/x-icon"/>
+	<link href="/css/app.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center" >
+
+            <div class="col-xl-10 col-lg-12 col-md-9" style="margin-top:15vh">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0" >
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                     <img src="{{ asset('img/poslogo.png') }}" alt="" style="margin-top:20px;margin-left:25px;width:360px; height:360px"> 
+                    <div class="col-lg-6">
+                        <div class="p-5">
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
+                                <p class="mb-4">We get it, stuff happens. Just enter your email address below
+                                    and we'll send you a link to reset your password!</p>
+                            </div>
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
+                    <form method="POST" class="user" action="{{ route('password.email') }}">
                         @csrf
+                                <div class="form-group">
+                                    <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email Address">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-user btn-block">
                                     {{ __('Send Password Reset Link') }}
                                 </button>
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="/register">Create an Account!</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="/login">Already have an account? Login!</a>
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+
     </div>
-</div>
-@endsection
+
+</body>
+</html>
