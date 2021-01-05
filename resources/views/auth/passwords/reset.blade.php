44f3377585ee3439aa34699717f4ab8b660bb2cc
Kenneth Solomon <synecderz@gmail.com>
2021-01-06 00:54:25 +0800

    update design for login register email reset pages

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,19 +1,6 @@
-<html lang="en">
+@extends('layouts.login')
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>HRMIS</title>
-    <link rel="icon" href="{{ URL::asset('/img/favicon.ico') }}" type="image/x-icon"/>
-	<link href="/css/app.css" rel="stylesheet">
-
-</head>
-
-<body class="bg-gradient-primary">
+@section('content')
 
     <div class="container">
 
@@ -83,3 +70,4 @@
 
 </body>
 </html>
+@endsection
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>HRMIS</title>
+    <link rel="icon" href="{{ URL::asset('/img/favicon.ico') }}" type="image/x-icon"/>
+	<link href="/css/app.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+	@yield('content')
+</body>
+</html>
