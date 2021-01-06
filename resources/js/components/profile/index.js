657c1039dd8e55243d966c76114f98b35fc7d9db
JPGrefaldo <johnpaulgrefaldo@gmail.com>
2021-01-06 15:21:13 +0800

    sticky styles

---
+++ resources/js/components/profile/index.js
@@ -0,0 +1,21 @@
+import Education from './Education'
+import EmploymentStatus from './EmploymentStatus'
+import FamilyBackground from './FamilyBackground'
+import PersonalDetails from './PersonalDetails'
+import WorkExperience from './WorkExperience'
+import ProfileAddress from './ProfileAddress'
+import Eligibilities from './Eligibilities'
+import VoluntaryWork from './VoluntaryWork'
+import Training from './Training'
+
+export {
+	PersonalDetails,
+	WorkExperience,
+	Education,
+	EmploymentStatus,
+	FamilyBackground,
+	ProfileAddress,
+	Eligibilities,
+	VoluntaryWork,
+	Training
+}
